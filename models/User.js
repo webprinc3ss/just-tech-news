63901374c05773224bd23872c2adaf6f8ffff327
Mystic Kitty <djabranton@gmail.com>
2020-12-14 17:39:01 -0800

    Added pw hashing capability with bcrypt.

--- models/User.js
+++ models/User.js
@@ -1,8 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model {}
+class User extends Model { }
 
 // create fields/columns for User model
 User.init(
@@ -34,6 +35,18 @@ User.init(
     }
   },
   {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
