001a91e34070891c8c62207783c84c54dbe62e78
Mystic Kitty <djabranton@gmail.com>
2020-12-14 18:04:11 -0800

    Login method added.

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -13,6 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
