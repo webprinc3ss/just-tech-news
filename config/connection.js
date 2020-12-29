03996453aa3a3a6ab606ef068504be912854bec2
Mystic Kitty <djabranton@gmail.com>
2020-12-28 18:26:59 -0800

    Fixed Merge issues.

--- config/connection.js
+++ config/connection.js
@@ -2,26 +2,13 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-<<<<<<< HEAD
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-=======
 // create connection to our db
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
->>>>>>> develop
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
   });
-<<<<<<< HEAD
-}
-=======
->>>>>>> develop
 
 module.exports = sequelize;
--- models/Post.js
+++ models/Post.js
@@ -16,14 +16,7 @@ class Post extends Model {
           'post_url',
           'title',
           'created_at',
-<<<<<<< HEAD
-          [
-            sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-            'vote_count'
-          ]
-=======
           [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
->>>>>>> develop
         ],
         include: [
           {
